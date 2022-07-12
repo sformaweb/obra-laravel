5e1e82dccaa259281abf23f346921ffa31cf473c
sformaweb <sara.formaweb@gmail.com>
2022-07-12 08:57:50 +0200

    correcciones modo oscuro

--- resources/views/persoas/index.blade.php
+++ resources/views/persoas/index.blade.php
@@ -99,7 +99,7 @@
                                         @csrf
                                         {{ method_field('DELETE') }}
 
-                                        {{-- <button class="delete border-0 btn-link"
+                                         <button class="delete border-0 btn-link bg-transparent"
                                             onclick="return EliminarRegistro('Eliminar Persoa')">
                                             <svg xmlns="http://www.w3.org/2000/svg" alt="Eliminar rexistro" width="16"
                                                 height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
@@ -108,10 +108,8 @@
                                                 <path fill-rule="evenodd"
                                                     d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                             </svg>
-                                        </button> --}}
-                                        <button> <i class="bi bi-trash delete border-0  btn-link"
-                                                onclick="return EliminarRegistro('Eliminar Persoa')"></i>
                                         </button>
+
                                     </form>
 
                                 </td>
